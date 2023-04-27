923075799dabc727b3022a8743285426c5b54e80
mauthi <michael.mauthner@netwerker.at>
2023-04-27 17:17:35 +0200

    fix

--- src/Contwise/Contwise.php
+++ src/Contwise/Contwise.php
@@ -10,34 +10,34 @@ use Contwise\Resources\Protocol;
 class Contwise
 {
     private $connection;
-    private String $baseUrl = 'https://staging-tirol.mapservices.eu/nefos_app/';
 
     public function __construct($apiKey, $editApiKey, $debug = false)
     {
-        $this->connection = new Connection(['apiKey' => $apiKey, 'editApiKey' => $editApiKey, 'apiUrl' => $this->baseUrl, 'debug' => $debug]);
+        $baseUrl = $_ENV['CONTWISE_URL'];
+        $this->connection = new Connection(['apiKey' => $apiKey, 'editApiKey' => $editApiKey, 'apiUrl' => $baseUrl, 'debug' => $debug]);
     }
 
-    public function getConnection() :Connection
+    public function getConnection(): Connection
     {
         return $this->connection;
     }
 
-    public static function getObject() :self
+    public static function getObject(): self
     {
         return new self($_ENV['CONTWISE_API_KEY'], $_ENV['CONTWISE_EDIT_API_KEY']);
     }
 
-    public static function getMediumResource() :Medium
+    public static function getMediumResource(): Medium
     {
         return new Medium(self::getObject()->getConnection());
     }
-    
-    public static function getProtocolResource() :Protocol
+
+    public static function getProtocolResource(): Protocol
     {
         return new Protocol(self::getObject()->getConnection());
     }
 
-    public static function getFeaturesResource() :Feature
+    public static function getFeaturesResource(): Feature
     {
         return new Feature(self::getObject()->getConnection());
     }
